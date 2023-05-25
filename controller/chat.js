47ff82f19a4248e8e72bd8994f31274c3f3f0218
roy <464485682@qq.com>
2023-05-25 19:03:47 +0800

    feat

--- controller/chat.js
+++ controller/chat.js
@@ -1,36 +1,60 @@
+const { Configuration, OpenAIApi } = require('openai');
 const fetchSSE = require('../utils/fetchSSE');
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 exports.chat = async (req, res) => {
     try {
-        res.writeHead(200, {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            Connection: 'keep-alive',
+        // res.writeHead(200, {
+        //     'Content-Type': 'text/event-stream',
+        //     'Cache-Control': 'no-cache',
+        //     Connection: 'keep-alive',
+        // });
+        // const { model = 'gpt-3.5-turbo', messages } = req.body;
+        // await fetchSSE('https://api.openai.com/v1/chat/completions', {
+        //     onMessage: (data) => {
+        //         res.write(`data: ${data}\n\n`);
+        //     },
+        //     onError: (error) => {
+        //         res.error('10101', error.message);
+        //     },
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        //     },
+        //     body: JSON.stringify({
+        //         messages,
+        //         temperature: 1,
+        //         max_tokens: 256,
+        //         top_p: 1,
+        //         frequency_penalty: 0,
+        //         presence_penalty: 0,
+        //         model,
+        //         stream: true,
+        //     }),
+        // });
+        // const completion = await openai.createCompletion({
+        //     model,
+        //     temperature: 0,
+        //     max_tokens: 1536,
+        //     top_p: 1,
+        //     presence_penalty: 0,
+        //     frequency_penalty: 0,
+        //     messages,
+        // });
+        const completion = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: 'Hello world',
         });
-        const { model = 'gpt-3.5-turbo', messages } = req.body;
-        await fetchSSE('https://api.openai.com/v1/chat/completions', {
-            onMessage: (data) => {
-                res.write(`data: ${data}\n\n`);
-            },
-            onError: (error) => {
-                res.error('10101', error.message);
-            },
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-            },
-            body: JSON.stringify({
-                model,
-                temperature: 0,
-                max_tokens: 1536,
-                top_p: 1,
-                presence_penalty: 0,
-                frequency_penalty: 0,
-                messages,
-                stream: true,
-            }),
-        });
+        res.success(completion);
+        // const data = await fetch('https://api.openai.com/v1/chat/completions');
+        // res.success(data);
     } catch (error) {
         res.error('10101', error.message);
     }
