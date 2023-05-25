868c52b84415b9f860dff04cdcd58dd818f7cf5f
roy <464485682@qq.com>
2023-05-25 19:16:16 +0800

    feat

--- controller/chat.js
+++ controller/chat.js
@@ -54,7 +54,21 @@ exports.chat = async (req, res) => {
         //     model: 'text-davinci-003',
         //     prompt: 'Hello world',
         // });
-        console.log(completion);
+        completion.data.on('data', (data) => {
+            const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+            for (const line of lines) {
+                const message = line.replace(/^data: /, '');
+                if (message === '[DONE]') {
+                    return; // Stream finished
+                }
+                try {
+                    const parsed = JSON.parse(message);
+                    console.log(parsed.choices[0].text);
+                } catch (error) {
+                    console.error('Could not JSON parse stream message', message, error);
+                }
+            }
+        });
         res.success({});
         // const data = await fetch('https://api.openai.com/v1/chat/completions');
         // res.success(data);
