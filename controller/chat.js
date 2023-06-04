c63702772193bbccdfab36cc73b5b047c598b2ef
roy <464485682@qq.com>
2023-06-04 21:26:34 +0800

    feat

--- controller/chat.js
+++ controller/chat.js
@@ -7,11 +7,11 @@ const openai = new OpenAIApi(configuration);
 
 exports.chat = async (req, res) => {
     try {
-        res.writeHead(200, {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            Connection: 'keep-alive',
-        });
+        // res.writeHead(200, {
+        //     'Content-Type': 'text/event-stream',
+        //     'Cache-Control': 'no-cache',
+        //     Connection: 'keep-alive',
+        // });
         const { model = 'gpt-3.5-turbo', messages } = req.body;
         const completion = await openai.createChatCompletion(
             {
